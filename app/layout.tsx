ce726e69bb3df9acfc966a4ddbd0f4aa5e0f67e6
Maxime-Boue <maxime.boue@oclock.school>
2024-03-04 16:24:21 +0100

    add header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -16,10 +17,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-LightYellow text-gray-950 relative`}
+        className={`${inter.className} bg-[#FEFDFB] text-gray-950 relative h-[5000px]`}
       >
         <div className="bg-[#F2D1F3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#f3ebd1] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div
+          className="bg-[#f3ebd1] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] 
+        sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+        ></div>
+        <Header />
         {children}
       </body>
     </html>
