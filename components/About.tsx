ee2a8b6127be3e3432ed3f55ebb54b77edd135e4
Maxime-Boue <maxime.boue@oclock.school>
2024-03-08 15:54:03 +0100

    add About and SectionDiv component. Finish Intro component by adding some anchor

---
+++ components/About.tsx
@@ -0,0 +1,33 @@
+const About = () => {
+  return (
+    <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
+      <h2 className="text-3xl font-medium mb-8">About Me</h2>
+      <p className="mb-3">
+        I obtained a professional title following 6 months of intensive training
+        as a Fullstack JavaScript web developer at O&apos;Clock, as part of a
+        professional retraining program. My training enabled me to acquire solid
+        knowledge in{" "}
+        <span className="font-medium">
+          JavaScript/TypeScript, React, Node, Git, HTML/CSS
+        </span>
+        ... I also had the opportunity to develop my project management skills,
+        as a <span className="font-medium">Scrum Master</span>, within a 1-month
+        group project, developing from A to Z an{" "}
+        <span className="italic">
+          application for creating interactive stories
+        </span>
+        .
+      </p>
+      <p>
+        My motivation is boundless, fueled by a constant thirst to learn and
+        progress. Passionate about the digital world, technology and pop
+        culture, my scientific background and experience as a design technician
+        help me enormously in the understanding and logic so important in the
+        world of web development. So I&apos;m ready to invest time and effort in
+        mastering new languages, tools and frameworks.
+      </p>
+    </section>
+  );
+};
+
+export default About;
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import portrait from "@/public/portrait.jpg";
+import portrait from "@/public/astro.png";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Intro = () => {
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+    <section className="mb-28 max-w-[50rem] text- sm:mb-0">
       <div className="flex items-center justify-center">
         <div>
           <motion.div
@@ -24,12 +29,12 @@ const Intro = () => {
               height="200"
               quality="95"
               priority={true}
-              className="border-white border-[0.40rem] object-cover shadow-xl"
+              className="border-white border-[0.40rem] object-cover shadow-xl rounded-full"
             />
           </motion.div>
         </div>
       </div>
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-small !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -39,7 +44,45 @@ const Intro = () => {
         enjoy<span className="font-bold"> practice and create</span>
         <span className="italic"> sites & apps</span> Mostly in{" "}
         <span className="underline">React and NodeJS</span>
-      </motion.p>
+      </motion.h1>
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-3 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 shadow-lg outline-none focus:scale-110 hover:scale-110 hover:text-gray-700 hover:bg-white active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <IoIosArrowForward className="opacity-70 groupe-hover:translate-x-2 transition" />
+        </Link>
+        <a
+          className="group bg-white text-gray-700 border border-black/10 px-7 py-3 flex items-center gap-2 shadow-lg rounded-full outline-none focus:scale-110 hover:scale-110 hover:text-white hover:bg-gray-900 active:scale-105 transition"
+          href="/cv_maxime-boue.pdf"
+          download
+        >
+          Download my CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/maxime-boue-dev"
+          target="_blank"
+          className="bg-white text-gray-700 border border-black/10 p-4 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:text-gray-950  active:scale-105 transition"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/Maxime-Boue?tab=repositories"
+          target="_blank"
+          className="bg-white text-gray-700 border border-black/10 p-4 text-[1.35rem] flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:text-gray-950  active:scale-105 transition"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 };
