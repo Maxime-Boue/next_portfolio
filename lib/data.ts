48e6485ec9de3de13c723a48ccbe3d42f7e753c2
Maxime-Boue <maxime.boue@oclock.school>
2024-03-11 10:53:08 +0100

    finish proects section components

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,4 @@
-import corpcommentImg from "@/public/corpcomment.png";
+import blackhole from "@/public/blackhole.jpg";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 import React from "react";
@@ -62,21 +62,21 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "BlackHole",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    imageUrl: blackhole,
   },
   {
-    title: "rmtDev",
+    title: "DreamKrafters",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    tags: ["React", "TypeScript", "Node", "Tailwind", "Redux"],
     imageUrl: rmtdevImg,
   },
   {
-    title: "Word Analytics",
+    title: "AppMetea",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
